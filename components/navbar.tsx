67162894e2748172de0d50130aa96e925d09561f
modouaicha023 <modouaichadiop@gmail.com>
2025-03-31 13:56:30 +0000

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,23 +5,29 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 right-1 z-50 w-fit flex px-4 gap-x-2 py-2 m-2 items-center justify-center bg-red-100 rounded-2xl">
-      <Link href={"/"}>
-        <Image
-          src="/logo-no-bg.png"
-          alt="Modou Aicha Diop, modouaicha023, portfolio"
-          width={400}
-          height={400}
-          className="w-4 h-4 object-fill"
-        />
-      </Link>
-      <ul className="flex w-fit gap-x-2">
-        {navbarLinks.map((navbarLinks, index) => (
-          <Link href={navbarLinks.href} className="hover:underline" key={index}>
-            {navbarLinks.title}
-          </Link>
-        ))}
-      </ul>
-    </nav>
+    <div className="flex justify-end sticky top-0 z-50 w-full px-4 py-2 m-2">
+      <nav className="flex px-4 gap-x-2 py-2 items-center bg-red-100 rounded-2xl">
+        <Link href={"/"}>
+          <Image
+            src="/logo-no-bg.png"
+            alt="Modou Aicha Diop, modouaicha023, portfolio"
+            width={400}
+            height={400}
+            className="w-4 h-4 object-fill"
+          />
+        </Link>
+        <ul className="flex w-fit gap-x-2">
+          {navbarLinks.map((navbarLinks, index) => (
+            <Link
+              href={navbarLinks.href}
+              className="hover:underline"
+              key={index}
+            >
+              {navbarLinks.title}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
