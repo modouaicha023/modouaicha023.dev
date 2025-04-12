864c13c7957873a22c93fbfd3cb9bb1e2b3cbcdc
modouaicha023 <modouaichadiop@gmail.com>
2025-04-12 01:45:03 +0000

    add jsonLd to project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ProjectJsonLd } from "@/components/json-ld";
 import { projects } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,30 +20,33 @@ export default function ProjectsPage() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         {projects.map((project, index) => (
-          <Link
-            href={`/projects/${project.slug}`}
-            key={index}
-            className="group flex items-start gap-4 bg-white dark:bg-[#1a1a1a] border border-zinc-200 dark:border-zinc-800 rounded-lg p-4 hover:shadow-lg transition-shadow duration-300"
-          >
-            <div className="flex-shrink-0">
-              <Image
-                src={project.logo}
-                alt={project.name + " | Modou Aicha Diop"}
-                width={96}
-                height={96}
-                className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg object-cover"
-              />
-            </div>
+          <>
+            <ProjectJsonLd project={project} />
+            <Link
+              href={`/projects/${project.slug}`}
+              key={index}
+              className="group flex items-start gap-4 bg-white dark:bg-[#1a1a1a] border border-zinc-200 dark:border-zinc-800 rounded-lg p-4 hover:shadow-lg transition-shadow duration-300"
+            >
+              <div className="flex-shrink-0">
+                <Image
+                  src={project.logo}
+                  alt={project.name + " | Modou Aicha Diop"}
+                  width={96}
+                  height={96}
+                  className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg object-cover"
+                />
+              </div>
 
-            <div className="flex flex-col justify-between flex-1">
-              <h2 className="text-sm sm:text-xl font-semibold text-zinc-900 dark:text-white group-hover:underline">
-                {project.name}
-              </h2>
-              <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 sm:line-clamp-2 mt-2">
-                {project.description}
-              </p>
-            </div>
-          </Link>
+              <div className="flex flex-col justify-between flex-1">
+                <h2 className="text-sm sm:text-xl font-semibold text-zinc-900 dark:text-white group-hover:underline">
+                  {project.name}
+                </h2>
+                <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 sm:line-clamp-2 mt-2">
+                  {project.description}
+                </p>
+              </div>
+            </Link>
+          </>
         ))}
       </div>
     </section>
