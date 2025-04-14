25fe61e7f9f1da1d16428bbe3c400213ada6ae0e
modouaicha023 <modouaichadiop@gmail.com>
2025-04-14 17:25:42 +0000

    update

--- app/admin/projects/page.tsx
+++ app/admin/projects/page.tsx
@@ -1,31 +1,7 @@
 import { ProjectJsonLd } from "@/components/json-ld";
 import { projects } from "@/constants";
-import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-
-export const metadata: Metadata = {
-  title: "Mes Projects | Développeur Full Stack",
-  description:
-    "Portfolio de Modou Aicha Diop, développeur full stack spécialisé en React, Next.js et NestJS. Découvrez mes projets, compétences et services en développement web et mobile au Sénégal.",
-  openGraph: {
-    title: "Modou Aicha Diop | Développeur Full Stack au Sénégal",
-    description:
-      "Découvrez mon portfolio en tant que développeur full stack spécialisé en React, Next.js et NestJS. Je crée des solutions web et mobiles innovantes adaptées aux besoins des entreprises et startups.",
-    url: "https://www.modouaicha023.dev",
-    siteName: "Modou Aicha Diop - Portfolio",
-    images: [
-      {
-        url: "https://www.modouaicha023.dev/preview.png",
-        width: 1200,
-        height: 630,
-        alt: "Portfolio de Modou Aicha Diop - Développeur Full Stack",
-      },
-    ],
-    locale: "fr_FR",
-    type: "website",
-  },
-};
 
 export default function ProjectsPage() {
   return (
