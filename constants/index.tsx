68fbbc0687f3ef9125efd359273f0c95991acb00
modouaicha023 <modouaichadiop@gmail.com>
2025-04-14 22:08:35 +0000

    update project

--- constants/index.tsx
+++ constants/index.tsx
@@ -21,18 +21,6 @@ export const projects: Project[] = [
     type: "Freelance",
   },
   {
-    name: "Game Of Life",
-    slug: "game-of-life",
-    coverImage: "/projects/game-of-life.png",
-    logo: "/projects/game-of-life-logo.png",
-    stack: ["JavaScript", "HTML", "CSS"],
-    description:
-      "This is a game called Game of life challenge. The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.",
-    url: "https://modouaicha023.github.io/GameOfLife",
-    githubUrl: "https://github.com/modouaicha023/GameOfLife",
-    type: "Personal",
-  },
-  {
     name: "Galsen Mangas",
     slug: "galsen-mangas",
     coverImage: "/projects/galsen-mangas.png",
@@ -43,6 +31,18 @@ export const projects: Project[] = [
       "An app offering free access to anime, manga, and novels, packed with features for a seamless and enjoyable experience.",
     url: "https://galsen-mangas.vercel.app",
     githubUrl: "https://github.com/modouaicha023/galsen-mangas",
+    type: "Personal",
+  },
+  {
+    name: "Game Of Life",
+    slug: "game-of-life",
+    coverImage: "/projects/game-of-life.png",
+    logo: "/projects/game-of-life-logo.png",
+    stack: ["JavaScript", "HTML", "CSS"],
+    description:
+      "This is a game called Game of life challenge. The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.",
+    url: "https://modouaicha023.github.io/GameOfLife",
+    githubUrl: "https://github.com/modouaicha023/GameOfLife",
     type: "Personal",
   },
   // {
@@ -67,42 +67,42 @@ export const projects: Project[] = [
   //   url: "https://sama-keur-sn.onrender.com",
   //   githubUrl: "https://github.com/modouaicha023/sama-keur-sn",
   // },
-  {
-    name: "Jayy Ma Baguette",
-    slug: "jayy-ma-baguette",
-    coverImage: "/projects/jayy-ma-baguette.png",
-    logo: "/projects/jayy-ma-baguette-logo.png",
-    stack: ["HTML", "CSS"],
-    description:
-      "JayyMaBaguette is a landing page for a baker. It has 7 sections: the header, the category section, the about us section, the menu section, the product presentation section, the contact form, and the footer.",
-    url: "https://modouaicha023.github.io/jaaymabaguette",
-    githubUrl: "https://github.com/modouaicha023/jaaymabaguette",
-    type: "Personal",
-  },
-  {
-    name: "Sama Car",
-    slug: "sama-car",
-    coverImage: "/projects/sama-car.png",
-    logo: "/projects/sama-car-logo.png",
-    stack: ["React", "Tailwind CSS"],
-    description:
-      "Sama Car is a landing page for a car business. It is a React application, styled using Tailwind CSS. The landing page aims to showcase the car business and its offerings to potential customers in an engaging and visually appealing manner.",
-    url: "https://sama-car.vercel.app",
-    githubUrl: "https://github.com/modouaicha023/sama-car",
-    type: "Personal",
-  },
-  {
-    name: "Sen Coffee",
-    slug: "sen-coffee",
-    coverImage: "/projects/sen-coffee.png",
-    logo: "/projects/sen-coffee-logo.png",
-    stack: ["React", "Tailwind CSS"],
-    description:
-      "Sen Coffee is a homepage for a coffee shop. It is a React application styled with Tailwind CSS. The purpose of this homepage is to present the coffee business and its products in an attractive and visually appealing way to potential customers.",
-    url: "https://sen-coffee.netlify.app/",
-    githubUrl: "https://github.com/modouaicha023/sen-coffee",
-    type: "Personal",
-  },
+  // {
+  //   name: "Jayy Ma Baguette",
+  //   slug: "jayy-ma-baguette",
+  //   coverImage: "/projects/jayy-ma-baguette.png",
+  //   logo: "/projects/jayy-ma-baguette-logo.png",
+  //   stack: ["HTML", "CSS"],
+  //   description:
+  //     "JayyMaBaguette is a landing page for a baker. It has 7 sections: the header, the category section, the about us section, the menu section, the product presentation section, the contact form, and the footer.",
+  //   url: "https://modouaicha023.github.io/jaaymabaguette",
+  //   githubUrl: "https://github.com/modouaicha023/jaaymabaguette",
+  //   type: "Personal",
+  // },
+  // {
+  //   name: "Sama Car",
+  //   slug: "sama-car",
+  //   coverImage: "/projects/sama-car.png",
+  //   logo: "/projects/sama-car-logo.png",
+  //   stack: ["React", "Tailwind CSS"],
+  //   description:
+  //     "Sama Car is a landing page for a car business. It is a React application, styled using Tailwind CSS. The landing page aims to showcase the car business and its offerings to potential customers in an engaging and visually appealing manner.",
+  //   url: "https://sama-car.vercel.app",
+  //   githubUrl: "https://github.com/modouaicha023/sama-car",
+  //   type: "Personal",
+  // },
+  // {
+  //   name: "Sen Coffee",
+  //   slug: "sen-coffee",
+  //   coverImage: "/projects/sen-coffee.png",
+  //   logo: "/projects/sen-coffee-logo.png",
+  //   stack: ["React", "Tailwind CSS"],
+  //   description:
+  //     "Sen Coffee is a homepage for a coffee shop. It is a React application styled with Tailwind CSS. The purpose of this homepage is to present the coffee business and its products in an attractive and visually appealing way to potential customers.",
+  //   url: "https://sen-coffee.netlify.app/",
+  //   githubUrl: "https://github.com/modouaicha023/sen-coffee",
+  //   type: "Personal",
+  // },
 ];
 export const navbarLinks: NavbarLink[] = [
   {
