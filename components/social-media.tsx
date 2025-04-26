416e65e1b2dfff2f001354d64d516309a41336d4
modouaicha023 <modouaichadiop@gmail.com>
2025-04-26 00:35:24 +0000

    update link

--- components/social-media.tsx
+++ components/social-media.tsx
@@ -1,19 +1,12 @@
-"use client";
 import { socialLinks } from "@/constants";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function SocialMedia() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent));
-  }, []);
   return (
     <div className="flex flex-wrap w-full items-center justify-start gap-4">
       {socialLinks.map((social, i) => (
         <Link
-          href={isMobile ? social.mobile : social.href}
+          href={social.href}
           key={i}
           target="_blank"
           className="flex gap-x-1 items-center hover:underline hover:opacity-70"
