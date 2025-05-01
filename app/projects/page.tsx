6f5b8364f38b3f5fabc7094f9323ee60cceeeb48
modouaicha023 <modouaichadiop@gmail.com>
2025-05-01 01:25:29 +0000

    update UI

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -39,42 +39,91 @@ export default function ProjectsPage() {
           are the ones that I&apos;m most proud of.
         </p>
       </header>
+      <div className="mb-8">
+        <h2 className="text-2xl sm:text-3xl font-semibold tracking-tight text-zinc-800 dark:text-white mb-4">
+          Personnal projects
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {projects
+            .filter((project) => project.type === "Personal")
+            .map((project, index) => (
+              <div key={index}>
+                <ProjectJsonLd project={project} />
+                <Link
+                  href={`/projects/${project.slug}`}
+                  className="group flex items-start gap-4 bg-white dark:bg-[#1a1a1a] border border-zinc-200 hover:border-gray-400 dark:border-zinc-800 rounded-lg p-4 focus:outline-none focus:border-gray-400"
+                  tabIndex={0}
+                >
+                  <div className="flex-shrink-0">
+                    <Image
+                      src={project.logo}
+                      alt={project.name + " | Modou Aicha Diop"}
+                      width={96}
+                      height={96}
+                      className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg object-cover"
+                    />
+                  </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {projects.map((project, index) => (
-          <div key={index}>
-            <ProjectJsonLd project={project} />
-            <Link
-              href={`/projects/${project.slug}`}
-              className="group flex items-start gap-4 bg-white dark:bg-[#1a1a1a] border border-zinc-200 hover:border-gray-400 dark:border-zinc-800 rounded-lg p-4 focus:outline-none focus:border-gray-400"
-              tabIndex={0}
-            >
-              <div className="flex-shrink-0">
-                <Image
-                  src={project.logo}
-                  alt={project.name + " | Modou Aicha Diop"}
-                  width={96}
-                  height={96}
-                  className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg object-cover"
-                />
+                  <div className="flex flex-col justify-between flex-1">
+                    <h2>
+                      <span className="text-sm sm:text-xl font-semibold text-zinc-900 dark:text-white group-hover:underline">
+                        {project.name}
+                      </span>
+                      <span className=" ml-2 font-mono text-xs sm:text-sm text-zinc-600 dark:text-zinc-400">
+                        {project.type}
+                      </span>
+                    </h2>
+                    <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 sm:line-clamp-2 mt-2">
+                      {project.description}
+                    </p>
+                  </div>
+                </Link>
               </div>
+            ))}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl sm:text-3xl font-semibold tracking-tight text-zinc-800 dark:text-white mb-4">
+          Freelance projects
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {projects
+            .filter((project) => project.type === "Freelance")
+            .map((project, index) => (
+              <div key={index}>
+                <ProjectJsonLd project={project} />
+                <Link
+                  href={`/projects/${project.slug}`}
+                  className="group flex items-start gap-4 bg-white dark:bg-[#1a1a1a] border border-zinc-200 hover:border-gray-400 dark:border-zinc-800 rounded-lg p-4 focus:outline-none focus:border-gray-400"
+                  tabIndex={0}
+                >
+                  <div className="flex-shrink-0">
+                    <Image
+                      src={project.logo}
+                      alt={project.name + " | Modou Aicha Diop"}
+                      width={96}
+                      height={96}
+                      className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg object-cover"
+                    />
+                  </div>
 
-              <div className="flex flex-col justify-between flex-1">
-                <h2>
-                  <span className="text-sm sm:text-xl font-semibold text-zinc-900 dark:text-white group-hover:underline">
-                    {project.name}
-                  </span>
-                  <span className=" ml-2 font-mono text-xs sm:text-sm text-zinc-600 dark:text-zinc-400">
-                    {project.type}
-                  </span>
-                </h2>
-                <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 sm:line-clamp-2 mt-2">
-                  {project.description}
-                </p>
+                  <div className="flex flex-col justify-between flex-1">
+                    <h2>
+                      <span className="text-sm sm:text-xl font-semibold text-zinc-900 dark:text-white group-hover:underline">
+                        {project.name}
+                      </span>
+                      <span className=" ml-2 font-mono text-xs sm:text-sm text-zinc-600 dark:text-zinc-400">
+                        {project.type}
+                      </span>
+                    </h2>
+                    <p className="text-xs sm:text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 sm:line-clamp-2 mt-2">
+                      {project.description}
+                    </p>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
-        ))}
+            ))}
+        </div>
       </div>
     </section>
   );
